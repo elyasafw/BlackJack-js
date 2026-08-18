208c8aaa6fac71c12134faf9c2def755f76414cb
elyasaf waisman <elyasafw1997@gmail.com>
2026-08-18 15:56:04 +0300

    created rounds repository and fixed logic in players repo

--- repository/players.repo.js
+++ repository/players.repo.js
@@ -3,22 +3,30 @@ import blackjackDB from "../db/db.config.js";
 
 const players = blackjackDB.collection("players");
 
-function playersRepo() {
+export function createPlayersRepo(collection) {
     async function createPlayer(player) {
-        const { insertedId } = await players.insertOne(player);
+        const { insertedId } = await collection.insertOne(player);
         return insertedId.toString();
     }
+
     async function updateChips(playerId, bet) {
-        const { chips } = await players.findOneAndUpdate(
+        const { chips } = await collection.findOneAndUpdate(
             { _id: new ObjectId(playerId) },
-            { $inc: { chips: -bet } },
+            { $inc: { chips: bet } },
             { returnDocument: "after" },
         );
         return chips;
     }
+
     async function findPlayer(playerId) {
-        const player = await players.findOne({ _id: new ObjectId(playerId) });
+        const player = await collection.findOne({
+            _id: new ObjectId(playerId),
+        });
         return player;
     }
     return { createPlayer, updateChips, findPlayer };
 }
+
+const playersRepo = createPlayersRepo(players);
+
+export default playersRepo;
---
+++ repository/rounds.repo.js
@@ -0,0 +1,55 @@
+import { ObjectId } from "mongodb";
+import blackjackDB from "../db/db.config.js";
+
+const rounds = blackjackDB.collection("rounds");
+
+export function createRoundsRepo(collection) {
+    async function createRound(round) {
+        const { insertedId } = await rounds.insertOne(round);
+        return insertedId.toString();
+    }
+
+    async function addCards(roundId, object) {
+        const updatedRound = await rounds.findOneAndUpdate(
+            { _id: new ObjectId(roundId) },
+            { $push: object },
+            { returnDocument: "after" },
+        );
+        return updatedRound;
+    }
+
+    async function addCardToPlayer(roundId, card) {
+        return addCards(roundId, { playerCards: card });
+    }
+
+    async function addCardToDealer(roundId, card) {
+        return addCards(roundId, { dealerCards: card });
+    }
+
+    async function updateStatus(roundId, status) {
+        await rounds.updateOne(
+            { _id: new ObjectId(roundId) },
+            { $set: { status } },
+        );
+    }
+
+    async function findRoundByPlayer(playerId) {
+        const round = await rounds.findOne({
+            playerId: playerId,
+            status: "in_progress",
+        });
+        return round;
+    }
+
+    return {
+        createRound,
+        addCardToPlayer,
+        addCardToDealer,
+        findRoundByPlayer,
+        updateStatus,
+    };
+}
+
+const roundsRepo = createRoundsRepo(rounds);
+
+export default roundsRepo;
